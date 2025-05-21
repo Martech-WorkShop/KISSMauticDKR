5e93560511d280eaab61caa23e10524edfdce239
Martech-WorkShop <martech.ws>
2025-05-21 02:47:25 +0200

    Release KISSMauticDKR for Mautic version 5.2.5

---
+++ assets/5.2.5/mautic-5.2.5-local.php
@@ -0,0 +1,15 @@
+<?php
+$parameters = array(
+	'db_driver' => 'pdo_mysql',
+	'db_host' => 'localhost',
+	'db_host_ro' => null,
+	'db_table_prefix' => null,
+	'db_port' => '3306',
+	'db_name' => 'mautic_db',
+	'db_user' => 'mautic_user',
+	'db_password' => 'Sq2n+FUVcfGsphSQkJI61BM0CoGs8xGKjl5ci/6qP7U=',
+	'db_backup_tables' => 1,
+	'db_backup_prefix' => 'bak_',
+	'secret_key' => '579fd73cdff58a37d206a8dc872658a20e552e8f0be6d46feef90698a661e000',
+	'site_url' => 'https://am-525.aiv.cx',
+);
